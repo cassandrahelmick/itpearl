4a1ca1a2cb0d9c783260fbe7c1fa9b0b1b01e75b
cassandrahelmick <cmhelmick@ualr.edu>
2025-02-04 21:22:02 +0000

    test

---
+++ circle/assets/script/circle.js
@@ -0,0 +1,41 @@
+"use strict";
+$( "CircleForm" ).validate( {
+
+}
+
+)
+
+ function CircleCalculations() {
+  if ($("#CircleForm").valid()) {
+  let radius; 
+  let circumference;
+  let radiusfp;
+  let diameter;
+  let area
+  radius = document.getElementById(radius).value;
+  radiusfp = parseFloat(radius)
+  diameter = calculateDiameter(radiusfp);
+  document.getElementById(diameter).innerHTML = diameter
+
+  circumference = calculateCircumference(radiusfp);
+  document.getElementById(circumference).innerHTML = circumference;
+
+  area = calculateArea(radiusfp);
+  document.getElementById(area).innerHTML = area;
+
+  let x = 1; 
+  }
+ }
+
+ function calculateDiameter(r) {
+  return 2 * r;
+ }
+
+ function calculateCircumference(r) {
+
+    return 2 * Math.PI * r
+ }
+
+ function calculateArea(r) {
+  return Math.PI * r * r 
+ }
